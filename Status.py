a097f0656f2dc9b7ccb7c32308cc01fd5940abf2
Stiizzy <75148429+Sskitz@users.noreply.github.com>
2021-04-30 21:23:52 -0700

    Create Status.py

---
+++ Status.py
@@ -0,0 +1,23 @@
+import discord
+from itertools import cycle
+from discord.ext import commands, tasks
+
+status = cycle(['Add ur text here','ur text here','ur text here','ur text here'])  # you can add as much as you want EX: 'Stiizzy cat is hot','Name'
+
+bot = commands.Bot(command_prefix="!") # prefix will not be used for changng status
+
+@bot.event
+async def on_ready():
+    print("Changing Status started")
+    change_status.start()
+
+@tasks.loop(seconds=5) # change to how many secs you want - 5 is best
+async def change_status(): 
+
+ await bot.change_presence(activity=discord.Game(next(status)))
+
+bot.run("Your Token here", bot=False)
+
+
+#made by stiizzy cat #0001 
+# not my fault if you get disabled
